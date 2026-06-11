b190118eb8e02dbbf7fd9a120990cd8eb847c4f2
zeyad salim <ziadsalim121@gmail.com>
2026-06-12 02:55:56 +0300

    setup for portfolio

---
+++ app.js
@@ -0,0 +1,4 @@
+// GSAP plugins are already loaded via CDN in the HTML file
+const gsap = window.gsap // Declare gsap variable
+const ScrollTrigger = window.ScrollTrigger // Declare ScrollTrigger variable
+
